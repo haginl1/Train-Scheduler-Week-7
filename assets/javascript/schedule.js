921845c6dc0c3f16ce999cc447c4206b5df5a359
Lisa Hagin <lhagin@encompass.tv>
2017-03-14 09:29:41 -0400

    tryng to fix table append

--- assets/javascript/schedule.js
+++ assets/javascript/schedule.js
@@ -12,58 +12,14 @@ var trainschedule;
   };
   
   firebase.initializeApp(config);
-  trainschedule= firebase.database();
+ 
 
-  var ref = trainschedule.ref("trains")
-  
-  ref.on('value', gotData);
 
-  function gotData(data){
-	  
-	  var trains = data.val();
-	  var keys = Object.keys(trains);
-	  for (var i=0; i < keys.length; i++){
-		  
-		var k = keys[i];
-		var trainName = trains[k].name;
-		var destination = trains[k].destination;
-		var firstTrainTime = trains[k].firstTrain;
-		console.log(firstTrainTime);
-		var frequency = trains[k].frequency;
-		
-		var currentTime = moment();
-		console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-		
-		var diffTime = moment().diff(moment.unix(firstTrainTime), "minutes");
-		console.log("DIFFERENCE IN TIME: " + diffTime);
-		
-		var tRemainder = diffTime % frequency;
-		console.log(tRemainder);
-
-		// Minute Until Train
-		var tMinutesTillTrain = frequency - tRemainder;
-		console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-		console.log(frequency);
-		console.log(tRemainder);
-
-		// Next Train
-		var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-		console.log("ARRIVAL TIME: " + moment(nextTrain).format("HH:mm"));
-		var list = trains[k];
-			console.log(list);
-
-		//add the new train to the list
-		var tableEntry = $('<tr>');
-		 tableEntry.append('<td>'+ list.name +'</td>')
-		  .append('<td>'+ list.destination + '</td>')
-		  .append('<td>'+ list.frequency + '</td>')
-		  .append('<td>'+ moment(nextTrain).format("HH:mm") + '</td>')
-		  .append('<td>'+ tMinutesTillTrain +'</td>')
-		  $('#trainsList').append( tableEntry );  
-	  }
-  }
-	//connect to the submit button to push the new train up to firebase///
+//connect to the submit button to push the new train up to firebase///
 	$("#addTrain").on("click", function(){
+		
+		// Don't refresh the page!
+        event.preventDefault();
 
 		//get the user inputs from the form
 		var trainName = $("#nameInput").val().trim();
@@ -79,15 +35,75 @@ var trainschedule;
 			firstTrain: firstTrainTime,
 			frequency: frequency,
 		};
-		
+		//send the new train to the database
 		ref.push(newTrain);
+		
 		//clear out the text boxes and ready for a new train
 		$("#nameInput").val("");
 		$("#destinationInput").val("");
 		$("#timeInput").val("");
 		$("#frequencyInput").val("");
-
+		$("#trainBody").empty;
 	});
+		//naming the database
+		trainschedule= firebase.database();
+		//shortening the db name and creating the trains node
+		var ref = trainschedule.ref("trains")
+
+		ref.on('value', gotData);
+		function gotData(data){
+			var trains = data.val();
+			var keys = Object.keys(trains);
+			//loop through the trains and get their values
+			for (var i=0; i < keys.length; i++){
+				var k = keys[i];
+				var trainName = trains[k].name;
+				var destination = trains[k].destination;
+				var firstTrainTime = trains[k].firstTrain;
+				var frequency = trains[k].frequency;
+				//calcualte the times to next train for each train
+				var currentTime = moment();
+				console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+				
+				var diffTime = moment().diff(moment.unix(firstTrainTime), "minutes");
+				console.log("DIFFERENCE IN TIME: " + diffTime);
+				
+				var tRemainder = diffTime % frequency;
+				console.log(tRemainder);
+
+				// Minute Until Train
+				var tMinutesTillTrain = frequency - tRemainder;
+				console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+				console.log(frequency);
+				console.log(tRemainder);
+
+				// Next Train
+				var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+				console.log("ARRIVAL TIME: " + moment(nextTrain).format("HH:mm"));
+				var list = trains[k];
+					console.log(list);
+
+				//add the new train to the list
+				
+				$("#trainsList").append(
+					"<tr>" +
+					"<td>" + list.name  + "</td>" +
+					"<td>" + list.destination + "</td>" +
+					"<td>" + list.frequency + "</td>" +
+					"<td>" + moment(nextTrain).format("HH:mm") + "</td>" +
+					"<td>" + tMinutesTillTrain + "</td>" + "</tr>"
+					);
+				
+				// var tableEntry = $('<tr>');
+				// tableEntry.append('<td>'+ list.name +'</td>')
+				// .append('<td>'+ list.destination + '</td>')
+				// .append('<td>'+ list.frequency + '</td>')
+				// .append('<td>'+ moment(nextTrain).format("HH:mm") + '</td>')
+				// .append('<td>'+ tMinutesTillTrain +'</td>')
+				// $('#trainsList').append( tableEntry );  
+			}
+		}
+	
 
 
   
